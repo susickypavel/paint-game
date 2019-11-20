0f5c3af399f2783204b6d507dab3e63774d09f8a
Jan Susicky <susicky.jan@gmail.com>
2019-11-20 22:43:29 +0100

    feat: three js init

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,31 @@
-console.log('lul');
+import { BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+
+const { innerWidth, innerHeight } = window;
+
+// Initialize renderer and corresponding DOM element.
+const renderer = new WebGLRenderer();
+renderer.setSize(innerWidth, innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Create scene and camera
+const scene = new Scene();
+const camera = new PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
+
+// Create a cube
+const geometry = new BoxGeometry(1, 1, 1);
+const material = new MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new Mesh(geometry, material);
+
+// Add cube to the scene
+scene.add(cube);
+
+// Move the camera lil bit to see the cube.
+camera.position.z = 5;
+
+// Render loop
+function render(): void {
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+}
+
+render();
