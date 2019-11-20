82b9c627c2a7e99ec9749309bff38102ae9e1403
Jan Susicky <susicky.jan@gmail.com>
2019-11-20 22:01:55 +0100

    chore: workspace init

---
+++ src/main.ts
@@ -0,0 +1,1 @@
+console.log('lul');
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+
+    entry: './src/main.ts',
+
+    output: {
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    devtool: 'cheap-module-eval-source-map',
+
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+
+    module: {
+        rules: [
+            { test: /\.ts$/, use: 'ts-loader' }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html'
+        })
+    ],
+
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    }
+};
